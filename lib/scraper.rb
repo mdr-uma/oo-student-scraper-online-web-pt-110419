91c8ef11bb6ebd4a5026b69c53860e91e494f734
Uma Manandhar <um_2003_8@hotmail.com>
2020-01-16 05:10:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -43,7 +43,7 @@ class Scraper
     web_page = Nokogiri::HTML(open(profile_url))
     doc = web_page.css("div.social-icon-container").children.css("a")
     profile_links = doc.map do |links|
-      links.values
+      links.value
     end
     profile_links.flatten.each do |link|
       if link.include?("linkedin")
